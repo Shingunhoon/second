3e0288ac5dc00f7631145b45fda7f39d0f1a3d2a
geonhoon <rjsgns01@naver.com>
2023-10-13 22:55:13 +0900

    first commit

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import { auth } from "@clerk/nextjs"
+
+const DashboardPage = () => {
+  const { userId } = auth()
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
+      <p className="mb-5">Welcome to Dashboard. logged-in user.</p>
+      <p>userId : {userId}</p>
+    </div>
+  )
+}
+export default DashboardPage
